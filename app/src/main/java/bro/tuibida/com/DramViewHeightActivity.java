3d5bfb971868a61345ee344e87d0b649aa7f5f85
shanz01 <shanz01@jumei.com>
2018-11-28 22:00:15 +0800

    动态设置 view margin

---
+++ app/src/main/java/bro/tuibida/com/DramViewHeightActivity.java
@@ -0,0 +1,13 @@
+package bro.tuibida.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DramViewHeightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dram_view_height);
+    }
+}
